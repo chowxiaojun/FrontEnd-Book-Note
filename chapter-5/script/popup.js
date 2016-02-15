9f36a96e738f54621db9cea0233070c4d9c5c4e3
ZhouXiaojun <snow041572@126.com>
2016-02-15 18:32:30 +0800

    第五章示例代码

---
+++ chapter-5/script/popup.js
@@ -0,0 +1,18 @@
+window.onload = prepareLinks;
+
+function prepareLinks() {
+    var links = document.getElementsByTagName("a");
+    alert(links.length);
+    for (var i = 0; i < links.length; i++) {
+        if (links[i].getAttribute("class") == "popup") {
+            links[i].onclick = function() {
+                popUp(this.getAttribute("href"));
+                return false;
+            }
+        }
+    }
+}
+
+function popUp(winURL) {
+    window.open(winURL, "popup", "width=320,height=480");
+}
