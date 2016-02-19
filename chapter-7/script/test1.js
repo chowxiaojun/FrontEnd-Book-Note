5cf8a7f52e73a5a76c54a4c9fdaeabbdf0600cf2
xiaojunzhou <chowxiaojun@126.com>
2016-02-19 23:16:33 +0800

    第七章示例代码

--- chapter-7/script/test1.js
+++ chapter-7/script/test1.js
@@ -9,3 +9,14 @@ function insertParagraph(text) {
     str += "</p>";
     document.write(str);
 }
+
+function insertParagraph1() {
+    // createElement方法
+    var para = document.createElement("p"); // 文档碎片  不是任何一棵DOM节点树的组成部分
+    // createTextNode方法   创建文本节点
+    var text = document.createTextNode("Hello world");
+    para.appendChild(text);
+    // appendChild方法
+    var testdiv = document.getElementById("testdiv");
+    testdiv.appendChild(para);
+}
